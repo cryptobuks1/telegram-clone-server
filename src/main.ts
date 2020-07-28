997cb95a689f19d01052e2a79debb5b7fd7e341c
monastyrskyi <vladyslav.monastyrskyi@gmail.com>
2020-07-28 16:07:11 +0300

    added [user, message, dialog, auth] modules/services/controllers/entities, config files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  await app.listen(PORT);
 }
-bootstrap();
+
+bootstrap().then(() => Logger.log(`Server running on http://localhost:${PORT}`, 'Server'));
---
+++ src/message/message.entity.ts
@@ -0,0 +1,40 @@
+import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  UpdateDateColumn,
+} from 'typeorm/index';
+
+import { Dialog } from '../dialog/dialog.entity';
+import { User } from '../user/user.entity';
+
+@Entity('messages')
+export class Message {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  @Column({ type: 'text', nullable: false })
+  content: string;
+
+  @CreateDateColumn()
+  createdDate: Date;
+
+  @UpdateDateColumn()
+  updatedDate: Date;
+
+  @ManyToOne(() => User, user => user.messages)
+  @JoinColumn({name: 'user_id', referencedColumnName: 'id'})
+  owner: User;
+
+  @JoinTable({
+    name: 'messages_dialogs',
+    joinColumn: { name: 'message_id', referencedColumnName: 'id' },
+    inverseJoinColumn: { name: 'dialog_id', referencedColumnName: 'id' },
+  })
+  @ManyToMany(() => Dialog, dialog => dialog.messages)
+  dialogs: Dialog[];
+}
